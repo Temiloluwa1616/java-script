50d6b5efb73180e841b67c0c660d8931de21a485
Temiloluwa1616 <sulaimantemiloluwa24@gmail.com>
2023-02-14 16:39:23 +0000

    first JS

---
+++ index.js
@@ -0,0 +1,16 @@
+  let myage = 18
+  let humandogratio=7
+  let mydogage= myage * humandogratio
+  console.log(mydogage)
+
+  let points=50
+  console.log(points)
+
+  points=points+50
+  console.log(points)
+
+  points=points-75
+  console.log(points)
+
+  points=points+45
+  console.log(points)
